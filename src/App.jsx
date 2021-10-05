964c799366bacade14348c05d07bef938381ab27
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-10-05 11:19:10 +0530

    feat: basic parallax effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState();
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
 
-  const handleScroll = () => setCount(window.pageYOffset);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-  return <div className="App"></div>;
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <>
+      <section className="Parallax">
+        <div
+          className="Parallax-image Parallax-sky-background"
+          style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+        />
+        <div
+          className="Parallax-image Parallax-landscape-foreground"
+          style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+        />
+        <div
+          className="Parallax-content"
+          style={{ transform: `translateY(-${offsetY * 0.2}px)` }}
+        >
+          <div className="heading">TREKK</div>
+        </div>
+      </section>
+      <section className="Parallax">
+        <div
+          className="Parallax-image Parallax-brown-background"
+          style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+        />
+      </section>
+    </>
+  );
 }
 
 export default App;
